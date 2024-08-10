0834440ba47eff10ce5954e60beab4d01313749f
Jesus Gaitan <166076219+JesusG16@users.noreply.github.com>
2024-08-09 21:35:09 -0500

    Added styling and section Added basic css styling and the profesional path section.

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const translations = {
         about: "About me",
         skills: "Skills",
         projects: "Projects",
-        contact: "Contact",
-        tagline: "Freelance data scientist specialized in data visualization",
+        journey: "Profesional Path",
+        tagline: "Freelance data scientist, specialized in data visualization",
+        github: "Check my GitHub",
+        email: "Send me an email",
         aboutMe: `Data Scientist with a background in physics and mathematics. 
         Currently working on informetrics with a strong emphasis on data visualization, 
         I'm open to freelance opportunities in data science or programming. 
@@ -22,10 +24,21 @@ const translations = {
         project2Title: "Project 2",
         project2Desc: "Coming soon",
         viewProject: "View Project",
-        nameLabel: "Name:",
-        emailLabel: "Email:",
-        messageLabel: "Message:",
-        sendButton: "Send",
+        aboutJourney: `It would be very naive of me to consider I'm at the peak of any of my skills, as I am still at the beginning 
+        of my professional career. However, I am confident in my ability to effectively apply the skills I have acquired so far. 
+        This section documents part of my journey, including the development of my initial projects, the courses 
+        I am currently taking, and the new skills I am working to acquire.`,
+        upcomingProjectsTitle: `Upcoming projects`,
+        coursesTitle: `Courses`,
+        skillProgressTitle: `New skills`,
+        upcomingProjects: `I have already decided my first portfolio project, it's going to be centered around
+        data visualization. So far I've determined the data I'm using (historic global temperature changes), the
+        main tool will be dc.js and I'm halfway trough a design. Hopefully I'll be able to start a page for that
+        and link it here before the end of August.`,
+        courses: `I'm currently doing courses on:`,
+        courses1: `Research Management with an emphasis on Science, Technology, and Innovation Programs and Projects, at Universidad del Valle (Colombia)`,
+        courses2: `Django web framework, as part of Meta Back-End Developer Professional Certificate`,
+        skillProgress: `I'm using roadmap.sh to learn about software design and architecture`,
         rightsReserved: `Licensed under CC BY-SA 4.0. You are free to share and adapt the content as long as 
         you give appropriate credit and distribute any derivative works under the same license.`
     },
@@ -34,8 +47,10 @@ const translations = {
         about: "Sobre mí",
         skills: "Habilidades",
         projects: "Proyectos",
-        contact: "Contacto",
-        tagline: "Científico de datos freelance especializado en visualización de datos",
+        journey: "Camino profesional",
+        tagline: "Científico de datos, especializado en visualización de datos",
+        github: "Enlace a mi GitHub",
+        email: "Envíame un email",
         aboutMe: `Científico de datos con formación en física y matemáticas. Actualmente trabajo en 
         informetría con énfasis en visualización de datos, y estoy abierto a trabajar en otros proyectos 
         relacionados a datos o programación como freelance. Mi formación científica me ha dotado de fuertes 
@@ -52,10 +67,21 @@ const translations = {
         project2Title: "Proyecto 2",
         project2Desc: "Disponible próximamente",
         viewProject: "Ver Proyecto",
-        nameLabel: "Nombre:",
-        emailLabel: "Correo electrónico:",
-        messageLabel: "Mensaje:",
-        sendButton: "Enviar",
+        aboutJourney: `Sería ingenuo de mi parte considerar que tengo un dominio completo de todas mis habilidades, aún 
+        más considerando que estoy iniciando mi carrera profesional. Sin embargo, confío en mi capacidad para aplicar eficazmente 
+        las habilidades que he adquirido hasta ahora. Esta sección documenta parte de mi trayectoria, incluyendo el desarrollo de mis primeros proyectos, 
+        los cursos que estoy realizando y las nuevas habilidades que estoy en proceso de adquirir.`,
+        upcomingProjectsTitle: `Proyectos futuros`,
+        coursesTitle: `Cursos`,
+        skillProgressTitle: `Nuevas habilidades`,
+        upcomingProjects: `Ya decidí mi primer proyecto para el portafolio, va a estar centrado en visualización de datos. 
+        Hasta el momento determiné los datos que voy a utilizar (cambios históricos en la temperatura global), 
+        la herramienta principal será dc.js y he podido completar aproximadamente la mitad del diseño. Espero 
+        poder comenzar una página para el proyecto y mostrar mis avances aquí antes de finales de agosto.`,
+        courses: `Actualmente estoy haciendo cursos en:`,
+        courses1: `Gestión de Investigación con Énfasis en Programas y Proyectos de Ciencia, Tecnología e Innovación, en la Universidad del Valle (Colombia)`,
+        courses2: `Marco web Django, como parte del certificado profesional de Desarrollador Backend de Meta`,
+        skillProgress: `Estoy usando roadmap.sh para aprender sobre desarrollo y arquitectura de software.`,
         rightsReserved: `Licenciado bajo CC BY-SA 4.0. Usted es libre de compartir y adaptar el contenido, 
         siempre que otorgue el crédito correspondiente y distribuya las obras derivadas bajo la misma licencia.`
     }
